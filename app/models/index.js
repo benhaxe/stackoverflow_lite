84b745191a0d2d32391218008dead7633c7b2d61
benhaxe <haxeboom@gmail.com>
2018-08-31 19:02:46 +0100

    feat(/authlogin): user login feature added

--- app/models/index.js
+++ app/models/index.js
@@ -2,7 +2,7 @@ import db from '../../config/db/psql';
 /*
     Database code for storing users informations. it stores the name, email and password of a user
 */
-export const UserModel = db.model('users', `CREATE TABLE IF NOT EXISTS USERS(
+exports.UserModel = db.model('users', `CREATE TABLE IF NOT EXISTS USERS(
   ID SERIAL PRIMARY KEY,
   NAME VARCHAR(255),
   EMAIL VARCHAR(255) NOT NULL UNIQUE,
@@ -10,44 +10,6 @@ export const UserModel = db.model('users', `CREATE TABLE IF NOT EXISTS USERS(
   PASSWORD VARCHAR(255) NOT NULL
  )`);
 
-/*
-  Database code for storing questions posted by the user. It stores, the question,
-   and the id of the creator of the question.
-  */
-export const QuestionModel = db.model('questions', `CREATE TABLE IF NOT EXISTS QUESTIONS(
-  ID SERIAL PRIMARY KEY,
-  QUESTION VARCHAR NOT NULL,
-  USERID INT,
-  CREATED_AT TIMESTAMP DEFAULT NOW(),
-  FOREIGN KEY (USERID) REFERENCES USERS(ID) ON DELETE CASCADE 
- )`);
-
-/*
-Database code for storing answers to questions. It stores the answer,
-number of upvote and downvote, and wether the answer is accepted or not
- */
-export const AnswerModel = db.model('answers', `CREATE TABLE IF NOT EXISTS ANSWERS(
-  ID SERIAL PRIMARY KEY,
-  ANSWER VARCHAR NOT NULL,
-  ACCEPTED BOOLEAN DEFAULT FALSE,
-  UPVOTE INT DEFAULT 0,
-  DOWNVOTE INT DEFAULT 0,
-  CREATED_AT TIMESTAMP DEFAULT NOW(),
-  USERID INT REFERENCES USERS(ID) ON DELETE CASCADE,
-  QUESTIONID INT REFERENCES QUESTIONS(ID) ON DELETE CASCADE
- )`);
-
-/*
-    Databae code for storing user comments on an answer. It stores the comment,
-    the id of the question and answer and the id of the person that post the comment
- */
-export const CommentModel = db.model('comments', `CREATE TABLE IF NOT EXISTS COMMENTS(
-  ID SERIAL PRIMARY KEY,
-  COMMENT TEXT NOT NULL,
-  CREATED_AT TIMESTAMP DEFAULT NOW(),
-  USERID INT REFERENCES USERS(ID) ON DELETE CASCADE,
-  ANSWERID INT REFERENCES ANSWERS(ID) ON DELETE CASCADE
- )`);
 
 // Exectute our database command
 db.sync();
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -4,7 +4,7 @@ import {
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import { app, server } from '../../app';
-import { UserModel } from '../../app/models';
+import { UserModel } from '../../app/models/index';
 
 const { expect } = chai;
 chai.use(chaiHttp);
@@ -15,110 +15,6 @@ after(() => {
 
 beforeEach((done) => {
   setTimeout(() => done(), 500);
-});
-
-describe('/auth/signup', () => {
-  before((done) => {
-    UserModel.delete()
-      .then(() => {
-        done();
-      })
-      .catch((e) => {
-        done(e);
-      });
-  });
-
-  it('should reply with 200 status code', (done) => {
-    chai
-      .request(app)
-      .post('/auth/signup')
-      .type('form')
-      .send({ email: 'easyclick05@gmail.com', password: 'abcdefgh' })
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        expect(res.status).to.equals(200);
-        expect(res.body).to.have.property('session').to.be.a('string');
-        return done();
-      });
-  });
-
-  it('should reply with 400 status code', (done) => {
-    chai
-      .request(app)
-      .post('/auth/signup')
-      .type('form')
-      .send({ email: 'easyclick05@gmail.com', password: 'abcdefgh' })
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        expect(res.status).to.equals(400);
-        expect(res.body)
-          .to.have.property('error')
-          .to.equal('User already exists');
-        return done();
-      });
-  });
-});
-
-describe('/auth/signup with invalid data', () => {
-  beforeEach((done) => {
-    UserModel.delete()
-      .then(() => {
-        done();
-      })
-      .catch((e) => {
-        done(e);
-      });
-  });
-
-  it('should reply with invalid email error message', (done) => {
-    chai
-      .request(app)
-      .post('/auth/signup')
-      .type('form')
-      .send({ email: 'easyclick@gmail', password: '62337087' })
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        expect(res.body)
-          .to.have.property('error')
-          .to.equals('Invalid email');
-        expect(res.status).to.equals(400);
-        return done();
-      });
-  });
-  it('should reply with 400 status code', (done) => {
-    chai
-      .request(app)
-      .post('/auth/signup')
-      .type('form')
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        expect(res.status).to.equals(400);
-        return done();
-      });
-  });
-  it('should reply with Missing credentials error message', (done) => {
-    chai
-      .request(app)
-      .post('/auth/signup')
-      .type('form')
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        expect(res.body)
-          .to.have.property('error')
-          .to.equals('Missing credentials');
-        return done();
-      });
-  });
 });
 
 describe('/auth/login', () => {
